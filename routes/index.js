b77605d6713b9f22220a89ca95c847ddcebe1831
ninhhuong <ninhhuongdst@gmail.com>
2017-06-08 07:31:32 +0700

    - implement checkin

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var io = require("socket.io").listen(server);
 server.listen(process.env.PORT || 3000);
 
 var ticket = require('../model/ticket');
+var token = require('../model/token');
 
 io.sockets.on('connection', function (socket) {
     console.log("someone connected");
@@ -14,6 +15,21 @@ io.sockets.on('connection', function (socket) {
             console.log(res);
             socket.emit('response open tickets', res);
         });
+    });
+	
+	socket.on('request create token', function(garage_id) {
+		token.add(garage_id, function (res) {
+            console.log(res);
+            // socket.emit('response create ticket', res);
+			//send to arduino
+        })
+	});
+
+    socket.on('request validate token', function(ticket_id, token_input) {
+        token.validate(ticket_id, token_input, function (res) {
+            console.log(res);
+            socket.emit('response validate ticket', res);
+        })
     });
 });
 
