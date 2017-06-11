ae4af74351829486dacbd3a807745412a52941dd
Hoàng Đức Long <nothing.noonedr@gmail.com>
2017-06-11 21:49:33 +0700

    Login, register not yet

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,29 @@ var io = require("socket.io").listen(server);
 server.listen(process.env.PORT || 3000);
 
 var ticket = require('../model/ticket');
+var account = require('../model/account');
 
+console.log("Start server");
 io.sockets.on('connection', function (socket) {
     console.log("someone connected");
-
+    //region ticket
     socket.on('request open tickets', function(account_token) {
+        console.log("server listen")
         ticket.getOpenTicketByAccount(account_token, function (res) {
             console.log(res);
             socket.emit('response open tickets', res);
         });
     });
+
+    //endregion
+
+    //Login request
+    socket.on('CheckEmailAndPassword', function (account_detail) {
+        var json = JSON.parse(account_detail);
+        account.login(json["Email"],json["Password"],function (res){
+            socket.emit('ResultLogin', res);
+        });
+    })
 });
 
 /* GET home page. */
