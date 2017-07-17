b39fc03cba450aba26e88e8a86ddefdc5121d202
Hoàng Đức Long <nothing.noonedr@gmail.com>
2017-07-18 02:00:19 +0700

    no message

--- model/account.js
+++ model/account.js
@@ -23,14 +23,37 @@ exports.register = function (email, password, roleID, callback) {
             }
 
             if (result.length === 0) {
-                sql = "INSERT INTO account(email, hash_password, token, roleID) VALUES ('" +
-                    email + "', '" + password + "', '" + token + "', '" + roleID + "')";
+                var temp = rand(16, 16);
+                sql = "INSERT INTO account(email, hash_password, token, roleID,reset_str) VALUES ('" +
+                    email + "', '" + password + "', '" + token + "', '" + roleID + "', '"+temp+"')";
                 client.query(sql, function (err) {
                     // db.endConnection();
                     if (err) {
                         return console.error('error running query', err);
                     }
-                    console.log("Register successful")
+                    console.log("Register successful");
+                    var mailOptions = {
+                        from: "Auto Car Park",
+                        to: email,
+                        subject: "Reset Password ",
+                        text: "Hello " + email + ".\nCode to validate account is " + temp + ".\n\nRegards,\nAuto Car-Park Team."
+                    };
+
+                    smtpTransport.sendMail(mailOptions, function (error) {
+                        if (error) {
+                            callback({
+                                'result': false,
+                                'data': {'mess': "Error"}
+                            });
+                            console.log(error);
+                        } else {
+                            callback({
+                                'result': true,
+                                'data': {'mess': "Successfull."}
+                            });
+                        }
+                    });
+
                     callback({'result': true, 'data': {'mess': "Successfully Registered"}});
                 });
             } else {
@@ -79,7 +102,7 @@ exports.reset_pass_init = function (email, callback) {
         if (err) {
             return console.error('error connecting', err);
         }
-        var temp = rand(24, 24);
+        var temp = rand(16, 16);
         client.query("SELECT * FROM account WHERE email = '" + email + "'", function (err, result) {
                 if (err) {
                     return console.error('error running query', err);
